34dbee4bc889d70df72d7358005b088a6e16052e
Aadam Kaivo <akaivo@ut.ee>
2019-04-27 17:14:37 +0300

    observable float

---
+++ BMbroker/Assets/ObservableFloat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ObservableFloat : ScriptableObject
+{
+    [SerializeField]
+    private float value = 0f;
+    public event Action<float> OnChanged;
+
+    public void set(float newValue)
+    {
+        value = newValue;
+        if (OnChanged != null)
+        {
+            OnChanged.Invoke(value);
+        }
+    }
+
+    public float get()
+    {
+        return value;
+    }
+}
