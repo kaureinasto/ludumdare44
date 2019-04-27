d980ad7b5d0f698e7097ef2f5e94380347a64ffa
Aadam Kaivo <akaivo@ut.ee>
2019-04-27 22:26:55 +0300

    BIIG Refactaaaa....fix

---
+++ BMbroker/Assets/Scripts/ShowNextTickGain.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowNextTickGain : MonoBehaviour
+{
+    private Text _text;
+    private List<IAgeEffector> ageEffectors = new List<IAgeEffector>();
+
+    private void Start()
+    {
+        _text = GetComponent<Text>();
+        StartCoroutine(FindEffectors());
+    }
+
+    private IEnumerator FindEffectors()
+    {
+        while (ageEffectors.Count == 0)
+        {
+            ageEffectors = FindObjectsOfType<MonoBehaviour>().OfType<IAgeEffector>().ToList();
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    private void Update()
+    {
+        float sum = 0f;
+        foreach (IAgeEffector ageEffector in ageEffectors)
+        {
+            sum += ageEffector.GetEffectPerTick();
+        }
+
+        _text.text = "Total effect: " + sum.ToString("F1");
+    }
+}
