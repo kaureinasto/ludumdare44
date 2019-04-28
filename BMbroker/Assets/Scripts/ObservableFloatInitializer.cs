2c30b0a30e2c48a55241225c4ce16fb9e7acbe77
Aadam Kaivo <akaivo@ut.ee>
2019-04-28 16:39:07 +0300

    Initialize ObservableFloat

---
+++ BMbroker/Assets/Scripts/ObservableFloatInitializer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class ObservableFloatInitializer : MonoBehaviour
+{
+    public ObservableFloat observableFloat;
+    public float initialValue;
+    
+    private void Start()
+    {
+        observableFloat.Set(initialValue);
+    }
+}
