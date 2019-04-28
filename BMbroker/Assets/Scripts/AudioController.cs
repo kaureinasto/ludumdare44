8fdc9b79f6d272177993fdcbc47c9b8eb219cacd
kaur <keinasto@gmail.com>
2019-04-28 12:05:45 +0300

    added audiocontroller

---
+++ BMbroker/Assets/Scripts/AudioController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour {
+	private AudioSource source;
+	public AudioClip buttonSound;
+	public AudioClip mainLoop;
+	// Use this for initialization
+	void Awake () {
+		source = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void playButtonClick(){
+		source.PlayOneShot(buttonSound,1f);
+	}
+
+}
