0c9b3de3a85b9f18c4cb464425121dbc9ddf5848
i-need-to-tell-you-something <toomassoomaa+github@gmail.com>
2019-04-28 19:53:37 +0300

    Small UI-related code changes.

--- BMbroker/Assets/Scripts/ageTextController.cs
+++ BMbroker/Assets/Scripts/ageTextController.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public class ageTextController : MonoBehaviour {
-	
+
 	private Text currentAge;
 	// Use this for initialization
 	public ObservableFloat age;
 	void Start() {
 		this.currentAge = GetComponent<Text>();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		this.currentAge.text = "You are " + this.age.Value() + " months old, which means you are " + this.age.Value()/12 + " years old.";
+		this.currentAge.text = "You are " + this.age.Value()/12 + " years old. \n(Equal to " + this.age.Value() + " months)";
 	}
-	
+
 }
