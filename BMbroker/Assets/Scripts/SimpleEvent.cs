ec0d00cf29fdbf3e194afaabfbd32e8eca1d923a
Aadam Kaivo <akaivo@ut.ee>
2019-04-29 14:39:24 +0300

    Ascend event

---
+++ BMbroker/Assets/Scripts/SimpleEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class SimpleEvent : ScriptableObject
+{
+
+	public event Action action;
+	
+	public void Fire()
+	{
+		if (action != null)
+		{
+			action.Invoke();
+		}
+	}
+}
