f6bd117bd32b4c20fea22a9cd7bd9dfcbdebb38e
kaur <keinasto@gmail.com>
2019-04-29 03:53:22 +0300

    added endgame sound, fixed issue

--- BMbroker/Assets/Scripts/GameSpawner.cs
+++ BMbroker/Assets/Scripts/GameSpawner.cs
@@ -23,13 +23,4 @@ public class GameSpawner : MonoBehaviour {
 		Destroy(currentGame);
 	}
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
