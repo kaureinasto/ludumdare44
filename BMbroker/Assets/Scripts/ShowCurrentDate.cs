fe2835b9e9e0c741d3091d8fbd3273981497f10c
i-need-to-tell-you-something <toomassoomaa+github@gmail.com>
2019-04-29 12:41:16 +0300

    Change date format.

--- BMbroker/Assets/Scripts/ShowCurrentDate.cs
+++ BMbroker/Assets/Scripts/ShowCurrentDate.cs
@@ -13,6 +13,6 @@ public class ShowCurrentDate : MonoBehaviour
   }
 
   void Update() {
-    this.currentDateText.text = "Current Date: " + this.currentDate.Value().ToString();
+    this.currentDateText.text = "Current Date: " + this.currentDate.Value().ToString("yyyy-MM");
   }
 }
