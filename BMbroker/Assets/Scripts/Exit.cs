85a3d166e23baf624b7dfbbf5a974bf267b6a83f
Aadam Kaivo <akaivo@ut.ee>
2019-04-29 14:30:31 +0300

    Add exit system

---
+++ BMbroker/Assets/Scripts/Exit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            ExitGame();
+        }
+    }
+
+    private void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
---
+++ BMbroker/Assets/Scripts/ExitDisplayer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using UnityEngine;
+
+public class ExitDisplayer : MonoBehaviour
+{
+    public GameObject exitDialogue;
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale == 1f)
+            {
+                Time.timeScale = 0f;
+                exitDialogue.SetActive(true);
+            }
+            else
+            {
+                Time.timeScale = 1f;
+                exitDialogue.SetActive(false);
+            }
+        } else if (Input.anyKeyDown && !Input.GetKeyDown(KeyCode.Y))
+        {
+                Time.timeScale = 1f;
+                exitDialogue.SetActive(false);
+        }
+    }
+}
