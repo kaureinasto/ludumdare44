61157e4b5b91d51c8f76da43e0a687fee45359cc
i-need-to-tell-you-something <toomassoomaa+github@gmail.com>
2019-04-29 10:57:57 +0300

    Change wording.

--- BMbroker/Assets/Scripts/ShowNextTickGain.cs
+++ BMbroker/Assets/Scripts/ShowNextTickGain.cs
@@ -10,7 +10,7 @@ public class ShowNextTickGain : MonoBehaviour
     public ObservableFloat income;
     public ObservableFloat costs;
     public Text text;
-    
+
     private void OnEnable()
     {
         UpdateText(0f);
@@ -25,18 +25,18 @@ public class ShowNextTickGain : MonoBehaviour
     private void UpdateText(float whatever)
     {
         float gain = income.Value() - costs.Value();
-        
+
         if(gain < 0){
-        text.text = "Rate: You are getting younger at a rate of: " + gain.ToString("F1")+ " per tick";
-        text.color = new Color(1, 0.4745098F, 0.7764706F, 1); //pink
+            text.text = "Rate: You are getting younger at a rate of: " + gain.ToString("F1") + " per month";
+            text.color = new Color(1, 0.4745098F, 0.7764706F, 1); //pink
         }
         if(gain == 0){
-        text.text = "Rate: You are not aging :O";
-        text.color = Color.white;
+            text.text = "Rate: You are not aging :O";
+            text.color = Color.white;
         }
         if(gain > 0){
-        text.text = "Rate: You are growing older at a rate of : " + gain.ToString("F1") +" per tick";
-        text.color = new Color(0.3137255F, 0.9803922F, 0.4823529F, 1); //green
+            text.text = "Rate: You are growing older at a rate of : " + gain.ToString("F1") + " per month";
+            text.color = new Color(0.3137255F, 0.9803922F, 0.4823529F, 1); //green
         }
     }
 }
