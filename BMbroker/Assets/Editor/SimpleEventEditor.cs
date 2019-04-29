ddebfed64617e529a0047ef3d3b6b533d68f071f
Aadam Kaivo <akaivo@ut.ee>
2019-04-29 15:46:45 +0300

    Add exit system

---
+++ BMbroker/Assets/Editor/SimpleEventEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(SimpleEvent))]
+public class SimpleEventEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (!EditorApplication.isPlaying) return;
+
+        SimpleEvent simpleEvent = (SimpleEvent) target;
+        if (GUILayout.Button("Fire!"))
+        {
+            simpleEvent.Fire();
+            Debug.LogWarning("Simple Event!");
+        }
+    }
+}
---
+++ BMbroker/Assets/Scripts/AscendAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AscendAnimation : MonoBehaviour
+{
+    public SimpleEvent startAscendEvent;
+
+    private void OnEnable()
+    {
+        startAscendEvent.action += forwardAscendToAnimator;
+    }
+
+    private void OnDisable()
+    {
+        startAscendEvent.action -= forwardAscendToAnimator;
+    }
+
+    private void forwardAscendToAnimator()
+    {
+        GetComponent<Animator>().SetBool("ascend", true);
+    }
+}
