d382affaa3662748cf1a5f19339658b20e207b8d
Aadam Kaivo <akaivo@ut.ee>
2019-04-29 13:10:42 +0300

    Animation flicker fix

---
+++ BMbroker/Assets/Scripts/ChangeAnimationByAge.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimationByAge : MonoBehaviour
+{
+    public ObservableFloat age;
+
+    private Animator _animator;
+    
+    private void OnEnable()
+    {
+        _animator = GetComponent<Animator>();
+        age.OnChanged += forwardAgeToAnimator;
+    }
+
+    private void OnDisable()
+    {
+        age.OnChanged -= forwardAgeToAnimator;
+    }
+
+    private void forwardAgeToAnimator(float newAge)
+    {
+        _animator.SetFloat("age", newAge);
+    }
+}
